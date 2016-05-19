4dc2c6384922f42c7328c6b400e20f71e4a5ae38
s-kimura <ostkimura1995@gmail.com>
2016-05-19 18:25:43 +0900

    indnet fix

--- 2/result.php
+++ 2/result.php
@@ -4,71 +4,71 @@ $result = myhtmlspecialchars($_POST);
 $category = array(1 => "企業について", 2 => "採用について", 3 => "ホームページについて", 4 => "その他");
 
 function myhtmlspecialchars($string) {
-  if (is_array($string)) {
-      return array_map("myhtmlspecialchars", $string);
-  } else {
-      return htmlspecialchars($string, ENT_QUOTES);
-  }
+    if (is_array($string)) {
+        return array_map("myhtmlspecialchars", $string);
+    } else {
+        return htmlspecialchars($string, ENT_QUOTES);
+    }
 }
 
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css" charset="utf-8">
-    <title>送信結果</title>
-  </head>
-  <body>
-    <div class="wrap">
-      <section>
-        <h1>送信結果</h1>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" charset="utf-8">
+        <title>送信結果</title>
+    </head>
+    <body>
+        <div class="wrap">
+            <section>
+            <h1>送信結果</h1>
 
-        <?php
-          echo '<h2>お客様情報</h2>';
-          echo '<table border="1">';
-          echo '<tbody>';
+            <?php
+            echo '<h2>お客様情報</h2>';
+            echo '<table border="1">';
+            echo '<tbody>';
 
-          echo '<tr><th>お名前</span></th>';
-          echo '<td>'.$result['name1'].' '.$result['name2'].'</td></tr>';
+            echo '<tr><th>お名前</span></th>';
+            echo '<td>'.$result['name1'].' '.$result['name2'].'</td></tr>';
 
-          echo '<tr><th>性別</span></th>';
-          echo '<td>'.$result['gender'].'</td></tr>';
+            echo '<tr><th>性別</span></th>';
+            echo '<td>'.$result['gender'].'</td></tr>';
 
-          echo '<tr><th>電話番号</span></th>';
-          echo '<td>'.$result['tel1'].'-'.$result['tel2'].'-'.$result['tel3'].'</td></tr>';
+            echo '<tr><th>電話番号</span></th>';
+            echo '<td>'.$result['tel1'].'-'.$result['tel2'].'-'.$result['tel3'].'</td></tr>';
 
-          echo '<tr><th>住所</span></th>';
-          echo '<td>'.$result['address'].'</td></tr>';
+            echo '<tr><th>住所</span></th>';
+            echo '<td>'.$result['address'].'</td></tr>';
 
-          echo '<tr><th>どこで知ったか</span></th>';
-          if(!isset($result['where'])){
-            echo '<td>選択なし</td></tr>';
-          }else{
-            echo '<td>';
-            $output = "";
-            foreach($result['where'] as $value) {
-              $output .= $value.' &amp ';
+            echo '<tr><th>どこで知ったか</span></th>';
+            if(!isset($result['where'])){
+                echo '<td>選択なし</td></tr>';
+            }else{
+                echo '<td>';
+                $output = "";
+                foreach($result['where'] as $value) {
+                    $output .= $value.' &amp ';
+                }
+                echo trim(trim($output), "&amp");
+                echo '</tr>';
             }
-            echo trim(trim($output), "&amp");
-            echo '</tr>';
-          }
-          echo '</tbody>';
-          echo '</table>';
-          echo '<h2>お問い合わせ内容</h2>';
+            echo '</tbody>';
+            echo '</table>';
+            echo '<h2>お問い合わせ内容</h2>';
 
-          echo '<table border="1">';
-          echo '<tbody>';
+            echo '<table border="1">';
+            echo '<tbody>';
 
-          echo '<tr><th>カテゴリ</th>';
-          echo '<td>'.$category[$result['num']].'</td></tr>';
+            echo '<tr><th>カテゴリ</th>';
+            echo '<td>'.$category[$result['num']].'</td></tr>';
 
-          echo '<tr class="question"><th>お問い合わせ内容</th>';
-          echo '<td><textarea class="area "rows="8" cols="60" disabled>'.$result['text'].'</textarea></td></tr>';
-          echo '</tbody>';
-          echo '</table>';
-        ?>
-      </section>
-    </div>
-  </body>
+            echo '<tr class="question"><th>お問い合わせ内容</th>';
+            echo '<td><textarea class="area "rows="8" cols="60" disabled>'.$result['text'].'</textarea></td></tr>';
+            echo '</tbody>';
+            echo '</table>';
+            ?>
+            </section>
+        </div>
+    </body>
 </html>
