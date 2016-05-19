7145cdc655ff5424f64f9b079cc82abb44f9e4ef
s-kimura <ostkimura1995@gmail.com>
2016-05-19 13:52:30 +0900

    form_test

---
+++ 1/form_test.php
@@ -0,0 +1,6 @@
+<form action="form_test2.php" method="get">
+
+  <input type="text" name="name1" value="">
+  <input type="submit">
+
+</form>
---
+++ 1/form_test2.php
@@ -0,0 +1,4 @@
+<?php
+echo $_GET['name1']."<br/ >";
+var_dump($_GET);
+?>
