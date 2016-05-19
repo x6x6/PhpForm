e38e659802070d88cfecea7985cb3823016213f4
s-kimura <ostkimura1995@gmail.com>
2016-05-19 18:01:18 +0900

    20160519 form

--- 2/contact.php
+++ 2/contact.php
@@ -9,8 +9,7 @@
       <div class="wrap">
         <section>
           <h1>お問い合わせフォーム</h1>
-
-          <form class="" action="check.php" method="post">
+          <form class="" action="result.php" method="post">
             <h2>お客様情報</h2>
             <table border="1">
               <tbody>
@@ -19,20 +18,20 @@
                 <tr>
                   <th>性別</th>
                   <td>
-                    <label><input type="radio" name="gender" value="1">男</label>
-                    <label><input type="radio" name="gender"　value="2">女</label>
-                    <label><input type="radio" name="gender"　value="3">その他</label>
+                    <label><input type="radio" name="gender" value="男" checked>男</label>
+                    <label><input type="radio" name="gender" value="女">女</label>
+                    <label><input type="radio" name="gender" value="その他">その他</label>
                   </td>
                 </tr>
                 <tr>
                   <th>電話番号<span class="need">[必須]</span></th>
                   <td>
-                    <input type="text" name="tel1" placeholder="090">-<input type="text" name="tel2" placeholder="2313">-<input type="text" name="tel3" placeholder="3512">
+                    <input type="text" name="tel1" placeholder="03"> - <input type="text" name="tel2" placeholder="3286"> - <input type="text" name="tel3" placeholder="4777">
                   </td>
                 </tr>
                 <tr>
                   <th>メールアドレス<span class="need">[必須]</span></th>
-                  <td><input type="text" name="email1" placeholder="example">@<input type="text" name="email2" placeholder="example.com"></td></tr>
+                  <td><input type="text" name="email1" placeholder="example"> @ <input type="text" name="email2" placeholder="example.com"></td></tr>
                 <tr><th>住所</th><td><input type="text" name="address" placeholder="東京都千代田区●●●1-1"></td></tr>
                 <tr>
                   <th>どこで知ったか</th>
@@ -48,9 +47,9 @@
             <table border="1">
               <tbody>
                 <tr>
-                  <th>質問カテゴリ<span class="need">[必須]</span></th>
+                  <th>カテゴリ<span class="need">[必須]</span></th>
                   <td>
-                    <select name="category">
+                    <select name="num">
                       <option value="1">企業について</option>
                       <option value="2">採用について</option>
                       <option value="3">ホームページについて</option>
@@ -58,14 +57,15 @@
                     </select>
                   </td>
                 </tr>
-                <tr class="question"><th>質問内容<span class="need">[必須]</span></th><td><textarea name="question" rows="8" cols="60"></textarea></td></tr>
+                <tr class="question"><th>お問い合わせ内容</th><td><textarea name="text" rows="8" cols="60"></textarea></td></tr>
 
               </tbody>
             </table>
-            <input class="submit" type="submit" value="送信内容の確認">
+
+
+            <input class="submit" type="submit" value="送信">
           </form>
         </section>
-
       </div>
     </body>
   </html>
